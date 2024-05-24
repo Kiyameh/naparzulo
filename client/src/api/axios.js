5d5ef70a5e25e3f9e3926ffb39fa32a20749a441
Kiyameh <kiyameh@outlook.com>
2024-05-24 11:16:50 +0200

    Actualizado Variable entorno Back end para prueba

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
-const URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000"
+//const URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000"
+const URL = "https://naparzulo-server.onrender.com"
 
 
 // AXIOS CONFIG: 
